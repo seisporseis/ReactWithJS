87d7ffc6f827f5dc9038840f257a0be7a53e95df
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2024-11-03 23:05:08 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,86 @@ import Guitar from "./components/Guitar";
 import { db } from "./data/db";
 
 export default function App() {
-  const [data, setData] = useState(db);
-  const [cart, setCart] = useState([]);
 
+  const initialCart = () => {
+    const localStorageCart  = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
+
+  const MIN_ITEMS = 1
+  const MAX_ITEMS = 5
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+  
   function addToCart(item) {
-    setCart(prevCart => [...prevCart, item]);
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if (itemExists >= 0) {
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if(item.id == id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if(item.id == id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
   }
 
   return (
-    
     <>
-      <Header />
+    <Header 
+      cart={cart}
+      removeFromCart={removeFromCart}
+      decreaseQuantity={decreaseQuantity}
+      increaseQuantity={increaseQuantity}
+      clearCart={clearCart}
+    />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
-        <div className="row mt-5">
-          { data.map((guitar) =>(
+        <div className="row mt-5 bg-guitars">
+          { data.map((guitar) => (
             <Guitar
               key={guitar.id}
               guitar={guitar}
               setCart={setCart} 
-              addToCart={addToCart}
+              addToCart={addToCart}              
             />          
           ))}
         </div>
